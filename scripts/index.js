16efc7a245abe2d534cdb0357bb0a7dcd2d37a59
Nafanya <Nafanya2.0@yandex.ru>
2025-04-02 22:54:24 +0300

    v.0.1_firstCommit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,43 @@
-// @todo: Темплейт карточки
+const container = document.querySelector('.content');
+const placesList = container.querySelector('.places__list');
+ 
+function deleteCard() {
+    const card = document.querySelector('.card')
+    card.remove();
+};
 
-// @todo: DOM узлы
+function addCard(cardContent, deleteElement) {             // функция создание карточек
+        
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardContent.link;
+    cardImage.alt = cardContent.name;
 
-// @todo: Функция удаления карточки
+    const cardTitle = cardElement.querySelector('.card__title');
+    cardTitle.textContent = cardContent.name;
 
-// @todo: Вывести карточки на страницу
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {deleteElement(cardElement)});
+
+    return cardElement;
+    
+};
+
+function deleteCard(cardElement) {      // функция удаление карточек
+    cardElement.remove();
+}
+
+for (let i = 0; i <= initialCards.length; i++) {         // добавляем карточки на страницу циклом
+    const card = addCard(initialCards[i], deleteCard);
+    placesList.append(card);
+ }; 
+ 
+ 
+ /*
+ initialCards.forEach(function(cardContent) {         // добавляем карточки на страницу методом forEach
+    const card = addCard(cardContent, deleteCard);
+    placesList.append(card);
+ }) 
+    */
